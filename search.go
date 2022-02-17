d7755eecca95e6bb4817e5c0f4a4a4e289e9d435
Jaspreet Singh <findjsk@gmail.com>
2022-02-17 22:31:42 +1100

    Loading data to structs

--- search.go
+++ search.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
+	"time"
 )
 
-var ticketsJson []byte
-var usersJson []byte
+type Ticket struct {
+	ID         string    `json:"_id"`
+	CreatedAt  time.Time `json:"created_at"`
+	Type       string    `json:"type"`
+	Subject    string    `json:"subject"`
+	AssigneeID int       `json:"assignee_id"`
+	Tags       []string  `json:"tags"`
+}
+
+type User struct {
+	ID        int       `json:"_id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	Verified  bool      `json:"verified"`
+}
+
+var tickets []Ticket
+var users []User
 
 func loadTickets() {
-	ticketsJson, _ = ioutil.ReadFile("./data/tickets.json")
-	usersJson, _ = ioutil.ReadFile("./data/users.json")
+	ticketsJson, _ := ioutil.ReadFile("./data/tickets.json")
+	usersJson, _ := ioutil.ReadFile("./data/users.json")
+
+	_ = json.Unmarshal(ticketsJson, &tickets)
+	_ = json.Unmarshal(usersJson, &users)
 }
 
 func init() {
